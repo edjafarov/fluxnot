60fd1dcd67a633bf71e8c9f009f366e37aea11f1
Eldar Djafarov <djkojb@gmail.com>
2015-01-04 22:10:39 +0200

    fix deps

--- server.js
+++ server.js
@@ -8,4 +8,28 @@ app.use(express.static("./assets"));
 
 app.use(clientAppMiddleware);
 
+
+app.get("/api/users", function(req, res){
+	res.json([
+	  {
+	    id: 0,
+	    name: "Bob",
+	    age: 23,
+	    bio: "stuff"
+	  },
+	  {
+	    id: 1,
+	    name: "Sally",
+	    age: 33,
+	    bio: "stuff1"
+	  },
+	  {
+	    id: 2,
+	    name: "John",
+	    age: 22,
+	    bio: "stuff2"
+	  }
+	]);
+})
+
 app.listen(3000);
